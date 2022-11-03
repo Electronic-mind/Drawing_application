f915847bbcd48f1e05ccfe7def89454e0c6de44c
Judi | Programmer <75368942+Electronic-mind@users.noreply.github.com>
2022-11-03 17:00:35 +0300

    Added a separator

--- drawing_using_mouse.py
+++ drawing_using_mouse.py
@@ -1,6 +1,6 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
-import sys
+
 
 # initialize pygame
 pygame.init()
@@ -10,9 +10,12 @@ clock = pygame.time.Clock()
 FPS = 200
 
 # Create the display
-WINDOW_SIZE = [800, 600]
-screen = pygame.display.set_mode(WINDOW_SIZE)
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), RESIZABLE)
 pygame.display.set_caption("Drawing app")
+
 
 # Colors reference
 WHITE = (255, 255, 255)
@@ -53,49 +56,54 @@ def brush(x, y):
 # The main loop
 while True:
 
+    pygame.draw.line(screen, BLACK, (0, WINDOW_HEIGHT - 33), (WINDOW_WIDTH, WINDOW_HEIGHT - 33))
     # Color buttons
     red_button = pygame.draw.rect(
-        screen, RED, (0, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, RED, (0, WINDOW_HEIGHT - 32, 32, 32))
     blue_button = pygame.draw.rect(
-        screen, BLUE, (32, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, BLUE, (32, WINDOW_HEIGHT - 32, 32, 32))
     green_button = pygame.draw.rect(
-        screen, GREEN, (64, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, GREEN, (64, WINDOW_HEIGHT - 32, 32, 32))
     cyan_button = pygame.draw.rect(
-        screen, CYAN, (96, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, CYAN, (96, WINDOW_HEIGHT - 32, 32, 32))
     yellow_button = pygame.draw.rect(
-        screen, YELLOW, (128, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, YELLOW, (128, WINDOW_HEIGHT - 32, 32, 32))
     magenta_button = pygame.draw.rect(
-        screen, MAGENTA, (160, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, MAGENTA, (160, WINDOW_HEIGHT - 32, 32, 32))
     white_button = pygame.draw.rect(
-        screen, WHITE, (192, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, WHITE, (192, WINDOW_HEIGHT - 32, 32, 32))
     black_button = pygame.draw.rect(
-        screen, BLACK, (224, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, BLACK, (224, WINDOW_HEIGHT - 32, 32, 32))
 
     # Brush buttons
     round_brush = pygame.draw.circle(
-        screen, BLACK, (WINDOW_SIZE[0] - 16, WINDOW_SIZE[1] - 16), 16, 0)
+        screen, BLACK, (WINDOW_WIDTH - 16, WINDOW_HEIGHT - 16), 16, 0)
     square_brush = pygame.draw.rect(
-        screen, BLACK, (WINDOW_SIZE[0] - 64, WINDOW_SIZE[1] - 32, 32, 32))
+        screen, BLACK, (WINDOW_WIDTH - 64, WINDOW_HEIGHT - 32, 32, 32))
 
     # Eraser button
     eraser_text = sys_font.render("Erase all", True, BLACK, WHITE)
     eraser_button = eraser_text.get_rect()
-    eraser_button.topright = (WINDOW_SIZE[0], 0)
+    eraser_button.topright = (WINDOW_WIDTH, 0)
 
     # Blitting the eraser
     screen.blit(eraser_text, eraser_button)
 
     for event in pygame.event.get():
+        
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+        if event.type == WINDOWMAXIMIZED:
+            screen.fill(WHITE)
+            
         if event.type == MOUSEBUTTONDOWN:
             mouse_x = event.pos[0]
             mouse_y = event.pos[1]
 
             # Check if the cursor
-            if mouse_y < WINDOW_SIZE[1] - 48:
+            if mouse_y < WINDOW_HEIGHT - 48:
                 # you could also load an image
                 brush(mouse_x, mouse_y)
 
@@ -134,15 +142,15 @@ while True:
             elif eraser_button.collidepoint(mouse_x, mouse_y):
                 screen.fill(WHITE)
 
+            
             surf = pygame.image.load("cursor2.png")
-            # and use that as your surface
-            colour = pygame.cursors.Cursor((16, 16), surf)
-            pygame.mouse.set_cursor(colour)
+            cursor = pygame.cursors.Cursor((16, 16), surf)
+            pygame.mouse.set_cursor(cursor)
 
         if event.type == MOUSEMOTION and event.buttons[0] == 1:
             mouse_x = event.pos[0]
             mouse_y = event.pos[1]
-            if mouse_y < WINDOW_SIZE[1] - 48:
+            if mouse_y < WINDOW_HEIGHT - 48:
                 brush(mouse_x, mouse_y)
 
     pygame.display.update()
